52478a059e20a3b4000343c62c2288f392727aae
Gabor Makrai <makraigabor@gmail.com>
2016-01-05 11:31:06 +0100

    contains code for calculating Weighted Reindeer Weariness

--- kaggle-santa-2015/submission.py
+++ kaggle-santa-2015/submission.py
@@ -1,3 +1,5 @@
+from haversine import haversine
+
 def loadOrder(fileName):
     print("Loading order from " + fileName + "...")
 
@@ -46,6 +48,28 @@ def doSubmissionFromOrder(order, gifts):
 #     print("counter: " + str(counter))
     
     return trips
+
+def calculateWRW(trips, gifts):
+    of = 0.0
+    for trip in trips:
+        tripWeight = 0.0
+        for gift in trip:
+            tripWeight = tripWeight + gift.weight
+        # add north pole to first gift weight
+        of = of + haversine((90.0, 0.0), (trip[0].latitude, trip[0].longitude)) * (tripWeight + 10.0)
+        # add last gift to north pole
+        of = of + haversine((trip[len(trip) - 1].latitude, trip[len(trip) - 1].longitude), (90.0, 0.0)) * (10.0)
+        previousGift = None
+        for gift in trip:
+            if previousGift == None:
+                previousGift = gift
+                tripWeight = tripWeight - previousGift.weight
+            else:
+                currentGift = gift
+                of = of + haversine((currentGift.latitude, currentGift.longitude), (previousGift.latitude, previousGift.longitude)) * (tripWeight + 10.0)
+                tripWeight = tripWeight - currentGift.weight
+                previousGift = currentGift
+    return of
 
 def createSubmissionFromTrips(trips, fileName):
     tripId = 1
